52ea9a5cdc21d3c99df334d60611641407b1800a
Matheus targa <matheus.targa@gmail.com>
2025-10-29 18:53:52 -0300

    iniciando o componente de funcionario

---
+++ Z-tintas/src/components/FormularioFuncionario/FormularioFuncionario.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import React from "react";
+//Importação componentes Bootstrap
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Image from "react-bootstrap/Image";
+
+//Importando a função useForm do pacote react-hook-form
+import { useForm } from "react-hook-form";
+
+const FormularioFuncionario = (props) => {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FormularioFuncionario

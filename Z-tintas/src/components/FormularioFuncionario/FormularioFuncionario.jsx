6f7217f8f1e1e6f27850ee8d87ddbba65b35f2e6
Matheus targa <matheus.targa@gmail.com>
2025-10-29 21:45:42 -0300

    estruturando o projeto na parte de funcionarios

--- Z-tintas/src/components/FormularioFuncionario/FormularioFuncionario.jsx
+++ Z-tintas/src/components/FormularioFuncionario/FormularioFuncionario.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import React from "react";
 //Importação componentes Bootstrap
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -11,11 +9,187 @@ import Image from "react-bootstrap/Image";
 //Importando a função useForm do pacote react-hook-form
 import { useForm } from "react-hook-form";
 
-const FormularioFuncionario = (props) => {
 
+const FormularioFuncionario = (props) => {
+//register = cria um objeto com os valores retirados dos inputs
+  //handleSubmit = envia os dados do formulario, caso dê erro ou sucesso
+  //formState {erros} = objetoque guarda uma lista de erros que aconteceram na tentativa de envio
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch
+  } = useForm();
   return (
-    <div>
-      
+    <div className='text-center'>
+      <Form>
+        <Row>
+          <Col>
+            <FloatingLabel controlId="FI-NOME" label="Nome" className="mb-5">
+              <Form.Control
+                type="text"
+                {...register("nome", {
+                  required: "O Nome é obrigatório",
+                  minLength: {
+                    value: 2,
+                    message: "O Nome deve ter pelo menos dois caracteres",
+                  },
+                  maxLength: {
+                    value: 10,
+                    message: "O Nome deve ter no máximo 10 caracteres",
+                  },
+                })}
+              ></Form.Control>
+              {errors.nome && <p className="error">{errors.nome.message}</p>}
+            </FloatingLabel>          
+          </Col>
+          <Col>
+            <FloatingLabel controlId="FI-CPF" label="CPF" className="mb-5">
+              <Form.Control
+                type="text"
+                {...register("cpf", {
+                  required: "O Nome é obrigatório",
+                  minLength: {
+                    value: 2,
+                    message: "O Nome deve ter pelo menos dois caracteres",
+                  },
+                  maxLength: {
+                    value: 10,
+                    message: "O Nome deve ter no máximo 10 caracteres",
+                  },
+                })}
+              ></Form.Control>
+              {errors.cpf && <p className="error">{errors.cpf.message}</p>}
+            </FloatingLabel>          
+          </Col>         
+        </Row>
+        <Row>
+          <Col>
+            <FloatingLabel controlId="FI-CARGO" label="Cargo" className="mb-5">
+              <Form.Control
+                type="text"
+                {...register("cargo", {
+                  required: "O cargo é obrigatório",
+                  minLength: {
+                    value: 2,
+                    message: "O cargo deve ter pelo menos dois caracteres",
+                  },
+                  maxLength: {
+                    value: 10,
+                    message: "O cargo deve ter no máximo 10 caracteres",
+                  },
+                })}
+              ></Form.Control>
+              {errors.cargo && <p className="error">{errors.cargo.message}</p>}
+            </FloatingLabel>           
+          </Col>
+          <Col>
+            <FloatingLabel controlId="FI-NUMERO" label="Numero" className="mb-5">
+              <Form.Control
+                type="text"
+                {...register("numero", {
+                  required: "O numero é obrigatório",
+                  minLength: {
+                    value: 2,
+                    message: "O numero deve ter pelo menos dois caracteres",
+                  },
+                  maxLength: {
+                    value: 10,
+                    message: "O numero deve ter no máximo 10 caracteres",
+                  },
+                })}
+              ></Form.Control>
+              {errors.numero && <p className="error">{errors.numero.message}</p>}
+            </FloatingLabel>           
+          </Col>
+        </Row>
+        <Row>
+            <FloatingLabel controlId="FI-EMAIL" label="E-mail" className="mb-5">
+              <Form.Control
+                type="text"
+                {...register("email", {
+                  required: "O email é obrigatório",
+                  minLength: {
+                    value: 2,
+                    message: "O email deve ter pelo menos dois caracteres",
+                  },
+                  maxLength: {
+                    value: 10,
+                    message: "O email deve ter no máximo 10 caracteres",
+                  },
+                })}
+              ></Form.Control>
+              {errors.email && <p className="error">{errors.email.message}</p>}
+            </FloatingLabel>              
+        </Row>
+        <Row>
+            <FloatingLabel controlId="FI-USUARIO" label="Usuário" className="mb-5">
+              <Form.Control
+                type="text"
+                {...register("usuario", {
+                  required: "O usuario é obrigatório",
+                  minLength: {
+                    value: 2,
+                    message: "O usuario deve ter pelo menos dois caracteres",
+                  },
+                  maxLength: {
+                    value: 10,
+                    message: "O usuario deve ter no máximo 10 caracteres",
+                  },
+                })}
+              ></Form.Control>
+              {errors.usuario && <p className="error">{errors.usuario.message}</p>}
+            </FloatingLabel>           
+        </Row>
+        <Row>
+          <Col>
+            <FloatingLabel controlId="FI-SENHA" label="Senha" className="mb-5">
+              <Form.Control
+                type="text"
+                {...register("senha", {
+                  required: "O senha é obrigatório",
+                  minLength: {
+                    value: 2,
+                    message: "O senha deve ter pelo menos dois caracteres",
+                  },
+                  maxLength: {
+                    value: 10,
+                    message: "O senha deve ter no máximo 10 caracteres",
+                  },
+                })}
+              ></Form.Control>
+              {errors.senha && <p className="error">{errors.senha.message}</p>}
+            </FloatingLabel>            
+          </Col>
+          <Col>
+            <FloatingLabel controlId="FI-CONFSENHA" label="Confirmação de Senha" className="mb-5">
+              <Form.Control
+                type="text"
+                {...register("confsenha", {
+                  required: "O campo é obrigatório",
+                  minLength: {
+                    value: 2,
+                    message: "O campo deve ter pelo menos dois caracteres",
+                  },
+                  maxLength: {
+                    value: 10,
+                    message: "O campo deve ter no máximo 10 caracteres",
+                  },
+                })}
+              ></Form.Control>
+              {errors.confsenha && <p className="error">{errors.confsenha.message}</p>}
+            </FloatingLabel>           
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="primary" size="lg" type="submit">
+              {props.page ==="editar" ? "Atualizar" : "Cadastrar"}
+            </Button>          
+          </Col>
+
+        </Row>
+      </Form>
     </div>
   )
 }
--- Z-tintas/src/pages/Funcionario/EditarFuncionario.jsx
+++ Z-tintas/src/pages/Funcionario/EditarFuncionario.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import FormularioFuncionario from "../../components/FormularioFuncionario/FormularioFuncionario";
+import { Container } from "react-bootstrap";
 
 const EditarFuncionario = () => {
   return (
     <div>
-      EDITAR FUNCIONARIO
+      <Container>
+        <h1 className="text-center">Editar Funcionarios</h1>
+        <FormularioFuncionario page="editar" />
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default EditarFuncionario
+export default EditarFuncionario;
